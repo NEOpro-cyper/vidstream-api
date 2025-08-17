bbf51806c4e576bd8ef38f808b9f9292d2be2f92
NEOpro-cyper <kaiskdishdisdi@gmail.com>
2025-08-17 18:15:39 +0530

    Update movieEpisodeServers.controller.ts

--- controllers/movieEpisodeServers.controller.ts
+++ controllers/movieEpisodeServers.controller.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
 import { Server } from "../types/controllers/movieEpisodeServers";
 import { ALLOWED_EPISODE_SERVERS } from "../config/vidstream";
-
 import { USER_AGENT_HEADER, ACCEPT_ENCODING_HEADER, ACCEPT_HEADER, SRC_AJAX_URL } from "../config/axois";
 import axios from "axios";
 import { load, CheerioAPI } from "cheerio";
@@ -11,20 +10,20 @@ export default async function (req: Request, res: Response) {
     const response = {
         servers: [] as Server[],
     };
-
+    
     const serverAjaxResponse = await axios.get(`${SRC_AJAX_URL}/movie/episode/servers/${req.query.episodeId}` as string, {
         headers: {
-            "Alt-Used": "vidstream.to",
-            "Host": "vidstream.to",
-            "Referer": `https://vidstream.to/series/${req.params.id}/${req.query.episodeId}`,
+            "Alt-Used": "flixhq.to",
+            "Host": "flixhq.to",
+            "Referer": `https://flixhq.to/series/${req.params.id}/${req.query.episodeId}`,
             "User-Agent": USER_AGENT_HEADER,
             "Accept-Encoding": ACCEPT_ENCODING_HEADER,
             Accept: ACCEPT_HEADER,
         },
     });
-
+    
     const $: CheerioAPI = load(serverAjaxResponse.data);
-
+    
     $(".dropdown-menu .dropdown-item").each((_, el) => {
         if (ALLOWED_EPISODE_SERVERS.filter(obj => obj === $(el).text().trim().toLowerCase())?.length === 0) return;
         response.servers.push({
@@ -32,6 +31,6 @@ export default async function (req: Request, res: Response) {
             name: $(el).text().trim(),
         });
     });
-
+    
     res.send(response);
 }
