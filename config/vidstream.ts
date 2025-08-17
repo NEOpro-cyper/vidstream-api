06ffe0d03d2da8e06652a34e4f7d94a3bc13db19
NEOpro-cyper <kaiskdishdisdi@gmail.com>
2025-08-17 18:09:19 +0530

    Update vidstream.ts

--- config/vidstream.ts
+++ config/vidstream.ts
@@ -1,4 +1,4 @@
-export const ALLOWED_EPISODE_SERVERS = ["upcloud", "vidcloud"];
+export const ALLOWED_EPISODE_SERVERS = ["upcloud", "vidcloud", "akcloud"];
 
 export const SERVERS = {
     RABBITSTREAM: "rabbitstream.net",
