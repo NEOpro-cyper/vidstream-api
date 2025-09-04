84369fbecd8712b4727e82183514b2170a0de141
NEOpro-cyper <kaiskdishdisdi@gmail.com>
2025-09-04 05:39:00 +0530

    Update movieDetails.controller.ts

--- controllers/movieDetails.controller.ts
+++ controllers/movieDetails.controller.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from "express";
 import { MovieDetailsResponse } from "../types/controllers/movieDetails";
+
 import { USER_AGENT_HEADER, ACCEPT_ENCODING_HEADER, ACCEPT_HEADER, SRC_AJAX_URL, SRC_BASE_URL } from "../config/axois";
 import axios from "axios";
 import { load, CheerioAPI } from "cheerio";
 import { extractDetect } from "../utils/extractMovieTvSeriesItem";
+
 // GET /movie/:id
 export default async function (req: Request, res: Response) {
     const response: MovieDetailsResponse = {
@@ -13,37 +15,45 @@ export default async function (req: Request, res: Response) {
         stats: [],
         related: [],
     };
-    const axiosResponse = await axios.get(${SRC_BASE_URL}/watch-movie/watch-${req.params.id} as string, {
+
+    const axiosResponse = await axios.get(`${SRC_BASE_URL}/watch-movie/watch-${req.params.id}` as string, {
         headers: {
             "Alt-Used": "vidstream.to",
             "Host": "vidstream.to",
-            "Referer": https://vidstream.to/watch-movie/watch-${req.params.id},
+            "Referer": `https://vidstream.to/watch-movie/watch-${req.params.id}`,
             "User-Agent": USER_AGENT_HEADER,
             "Accept-Encoding": ACCEPT_ENCODING_HEADER,
-            Accept: ACCEPTHEADER,
+            Accept: ACCEPT_HEADER,
         },
     });
+
     const $: CheerioAPI = load(axiosResponse.data);
+
     response.title = $(".movie-detail h3.movie-name").text();
     response.description = $(".movie-detail .is-description .dropdown-menu .dropdown-text").text().trim();
-    $(".movie-detail .is-sub > div").each((, el) => {
+
+    $(".movie-detail .is-sub > div").each((_, el) => {
         const stat: any = {
             name: $(el).find(".name").text(),
             value: []
         };
+
         const anchorTags = $(el).find(".value a");
         if (anchorTags.length) {
-            anchorTags.each((, anchor) => {
+            anchorTags.each((_, anchor) => {
                 stat.value.push($(anchor).text());
             });
         } else {
             stat.value = $(el).find(".value").text().trim();
         }
+
         response.stats.push(stat);
     });
-    $(".section-related .item").each((, el) => {
+
+    $(".section-related .item").each((_, el) => {
         response.related.push(extractDetect($, el));
     });
+
     const axiosResponseLines = axiosResponse.data.split("\n");
     for (let i = axiosResponseLines.length - 1; i > 0; i--) {
         if (axiosResponseLines[i].includes("const movie = {")) {
@@ -51,5 +61,6 @@ export default async function (req: Request, res: Response) {
             if (response.type === "movie") response.episodeId = axiosResponseLines[i + 4].split("'")[1];
         }
     }
+
     res.send(response);
-} 
+}
