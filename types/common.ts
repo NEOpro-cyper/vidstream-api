04ff68872136ec43f60d1d33bccb852030d35a0d
NEOpro-cyper <kaiskdishdisdi@gmail.com>
2025-09-06 08:14:42 +0530

    Update common.ts

--- types/common.ts
+++ types/common.ts
@@ -2,6 +2,7 @@ export interface MovieItem {
     id: string;
     title: string;
     poster: string;
+    tmdbId?: number; // Added TMDB ID
     stats: MovieItemStats;
 }
 
@@ -15,10 +16,12 @@ export interface TvSeriesItem {
     id: string;
     title: string;
     poster: string;
+    tmdbId?: number; // Added TMDB ID
     stats: TvSeriesItemStats;
 }
 
 interface TvSeriesItemStats {
     seasons: string;
+    episodes: string; // Fixed: This was missing in your original code
     rating: string;
 }
